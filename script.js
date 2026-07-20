7ae961c5d486ed3fc59425677b8e0905eb8fbaf9
ahaderide <arhaanbrothers39@gmail.com>
2026-07-20 09:36:47 -0700

    Enhance startRide function to include input validation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function startRide() {
-    alert("Welcome to Ahad E-Ride!");
+    const pickup = document.querySelectorAll("input")[0].value;
+    const drop = document.querySelectorAll("input")[1].value;
+
+    if (pickup === "" || drop === "") {
+        alert("Please enter Pickup and Drop Location");
+        return;
+    }
+
+    alert("Ride Booked!\n\nPickup: " + pickup + "\nDrop: " + drop);
 }
