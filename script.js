e0ba9311cdb9a53282c60adb993772567f9de1ec
ahaderide <arhaanbrothers39@gmail.com>
2026-07-20 11:10:44 -0700

    Implement mobile number validation in login function
    Added login function to validate mobile number input.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function startRide() {
+    function login() {
+    const mobile = document.getElementById("mobile").value;
+
+    if (mobile.length != 10) {
+        alert("Please enter a valid 10-digit mobile number");
+        return;
+    }
+
+    alert("Login Successful");
+}
     const pickup = document.querySelectorAll("input")[0].value;
     const drop = document.querySelectorAll("input")[1].value;
 
